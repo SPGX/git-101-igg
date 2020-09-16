4c6ebb00d101abd30f0e6efecee1e7e2d56fed34
SPGX <armsrichan2@gmail.com>
2020-09-16 13:38:35 +0700

    Update example.js

--- js/example.js
+++ js/example.js
@@ -1,1 +1,17 @@
-console.log{"Hello World"}
+let name = "A"
+names = "JOKE"
+let test = "Hello"
+
+let arr = ['a', 'b', 'c']
+console.log("arr: ", arr[1])
+
+let person = {
+    name: 'joke',
+    age: 13,
+    gender: {
+        name: 'male'    
+    },
+    arr: arr
+}
+console.log(person.name)
+console.log(person.gender)
